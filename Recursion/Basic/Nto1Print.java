e5b7ef63681e4f07e1c9523c4a1ad6b06d789bfc
satyamkodale <satyamkodale@gmail.com>
2024-06-16 19:08:21 +0530

    completed 2 examples of recurrsion

--- Recursion/Basic/Nto1Print.java
+++ Recursion/Basic/Nto1Print.java
@@ -17,6 +17,13 @@ public class Nto1Print {
          printBoth(5);
         System.out.println("----------------------------------------------------");
         System.out.println(factorial(5));
+        System.out.println("----------------------------------------------------");
+        System.out.println(digitsSum(1343));
+        System.out.println("----------------------------------------------------");
+        System.out.println(reverseNum(1343,0));
+        System.out.println("----------------------------------------------------");
+        System.out.println(reverseNum2(1343));
+        
         
 
     }
@@ -73,5 +80,52 @@ public class Nto1Print {
         
         return n*=factorial(n-1);
     }
+    
+    public static int digitsSum(int n)
+    {
+//        int r = n%10;
+//        if(n/10==0)
+//        {
+//           return n;
+//        }
+//        n=n/10;
+//        return r+digitsSum(n);
+        if(n==0)
+        {
+        return 0;
+        }
+        
+        return (n%10) + digitsSum(n/10);
+    }
+    public static int reverseNum(int num,int reverseNum)
+    {
+    
+        reverseNum=reverseNum*10+num%10;
+        if(num%10==num)
+        {
+            return reverseNum;
+        }
+        num=num/10;
+        return reverseNum(num,reverseNum);     
+    } 
+    
+    
+    public static int reverseNum2(int n)
+    {
+    
+        int digits= (int)(Math.log10(n))+1;
+        return helper( n , digits);  
+    }
+    
+    public static int helper(int n, int digits)
+    {
+        if(n%10==n)
+        {
+            return n;
+        }
+        return (n%10) * (int)Math.pow(10,digits-1)+helper(n/10,digits-1);
+
+    }
+    
     
 }
